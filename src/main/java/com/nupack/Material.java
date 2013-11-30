84d1671cfc6b0823e17a647ed3dd3ef0d2a1187b
starasov <tarasov.inf@gmail.com>
2013-11-30 00:00:05 -0500

    - Refined markups for Material model

--- src/main/java/com/nupack/Material.java
+++ src/main/java/com/nupack/Material.java
@@ -1,13 +1,25 @@
 package com.nupack;
 
+import java.math.BigDecimal;
+
 /**
  * User: starasov
  * Date: 11/29/13
  * Time: 11:45 PM
  */
 public enum Material {
-    FOOD,
-    DRUGS,
-    BOOKS
+    DEFAULT(new BigDecimal("0.0")),
+    FOOD(new BigDecimal("13.0")),
+    ELECTRONICS(new BigDecimal("2.0")),
+    PHARMACY(new BigDecimal("7.5"));
 
+    private final BigDecimal markup;
+
+    private Material(BigDecimal markup) {
+        this.markup = markup;
+    }
+
+    public BigDecimal getMarkup() {
+        return markup;
+    }
 }
