5adbf09f55f6aa3facd480298b8eb2e3c17c0a4c
starasov <tarasov.inf@gmail.com>
2013-11-30 01:54:06 -0500

    - Added material names support

--- src/main/java/com/nupack/Material.java
+++ src/main/java/com/nupack/Material.java
@@ -8,19 +8,33 @@ import org.jetbrains.annotations.NotNull;
  * Time: 11:45 PM
  */
 public enum Material {
-    DEFAULT(Markup.valueOf("0.0")),
-    FOOD(Markup.valueOf("13.0")),
-    ELECTRONICS(Markup.valueOf("2.0")),
-    PHARMACY(Markup.valueOf("7.5"));
+    DEFAULT("<default>", Markup.valueOf("0.0")),
+    FOOD("food", Markup.valueOf("13.0")),
+    ELECTRONICS("electronics", Markup.valueOf("2.0")),
+    DRUGS("drugs", Markup.valueOf("7.5"));
 
+    private final String name;
     private final Markup markup;
 
-    private Material(@NotNull Markup markup) {
+    private Material(@NotNull String name, @NotNull Markup markup) {
+        this.name = name;
         this.markup = markup;
+    }
+
+    @NotNull
+    public static Material findByName(@NotNull String name) {
+        for (Material material : values()) {
+            if (material.name.equals(name)) {
+                return material;
+            }
+        }
+
+        return DEFAULT;
     }
 
     @NotNull
     public Markup getMarkup() {
         return markup;
     }
+
 }
